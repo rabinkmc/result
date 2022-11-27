66a6caa9c400c2918ad54c29d50d812e67b41ccf
Rabin Dhamala <rabinkmc@gmail.com>
2022-11-27 18:32:11 +0545

    remove fs and add hint for diya to code.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker';
-import {writeFile} from 'node:fs';
+// import {writeFile} from 'node:fs';
 // const fs = require('fs');
 // const randomName = faker.name.fullName(); // Rowan Nikolaus
 // const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
@@ -27,8 +27,12 @@ function getStudentMarks() {
 }
 
 const marks = getStudentMarks();
-writeFile("marks.json", JSON.stringify(marks), 'utf8', (err)=>{
-  if (err) throw err;
-    console.log('The file has been saved!');
-});
+
+//to write to file
+// writeFile("marks.json", JSON.stringify(marks), 'utf8', (err)=>{
+//   if (err) throw err;
+//     console.log('The file has been saved!');
+// });
 console.log(marks);
+
+//write your code here 
